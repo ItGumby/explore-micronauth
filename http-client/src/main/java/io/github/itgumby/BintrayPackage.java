239a2cdb3150a376171a6f1a7f8b420f6facbe49
Brian Street <brian.c.street@gmail.com>
2019-07-21 15:03:50 -0600

    implemented https://guides.micronaut.io/micronaut-http-client/guide/index.html but without valid Bintray user/token

---
+++ http-client/src/main/java/io/github/itgumby/BintrayPackage.java
@@ -0,0 +1,22 @@
+package io.github.itgumby;
+
+public class BintrayPackage {
+    String name;
+    boolean linked;
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public boolean isLinked() {
+        return linked;
+    }
+
+    public void setLinked(boolean linked) {
+        this.linked = linked;
+    }
+}
